083724ffdb5148c95edf1ec9c6ef0c13d52db5c8
DaCostaSergio <47104470+DaCostaSergio@users.noreply.github.com>
2023-05-19 14:41:44 +0200

    change image

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import WeatherDataFetcher from './Components/WeatherDataFetcher'
 
 function App() {
   return (
-    <div className="bg-[url('../src/Assets/pexels-darius-krause-2182040.png')] bg-cover h-screen">
+    <div className="bg-[url('../src/Assets/pexels-darius-krause-2182040-2.jpg')] bg-cover h-screen">
       <NavHeader></NavHeader>
-      <div className='mx-5'>
+      <div className='mx-5 grid place-items-center'>
         <WeatherDataFetcher></WeatherDataFetcher>
       </div>
 
